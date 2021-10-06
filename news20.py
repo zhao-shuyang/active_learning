dfba910a979393cc733a736212af163fa0d7ebf2
lord.ouoh@gmail.com <shuyang.zhao@etut.fi>
2021-10-06 22:23:25 +0300

    implemented MAL1 without testing

--- news20.py
+++ news20.py
@@ -1,9 +1,10 @@
-import numpy as np 
+import collections
+
+import numpy as np
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
-import time
 
 import cluster
 
@@ -27,6 +28,7 @@ class ActiveLearner:
     def draw_next_batch(self):
         "The query strategy. This will be overriden by different active learning methods."
         s_indices = np.random.randint(0, len(self.U), self.batch_size)
+        self.n_batch += 1
         return self.U[s_indices]
 
     def annotate_batch(self, selection_batch, batch_target):
@@ -37,33 +39,80 @@ class ActiveLearner:
     def train(self):
         self.classifier.fit(self.X[self.L], self.y[self.L])
 
+
+class MAL1(ActiveLearner):
+    "This is a simple medoid-based active learner. Clustering is performed only once here"
+    def __init__(self, *args, **kwargs):
+        super(MAL1, self).__init__(*args, **kwargs)
+        self.P = np.array([])  # indices of instances with propagated labels
+        
+    @staticmethod
+    def compute_dist_mat(X):
+        """
+        The computation of distance matrix based on inner product.
+        It is equivalent to cosine distance when vector representations are normalized.
+        """
+        dist_mat = np.dot(X, X.T)
+        dist_mat = 1 - self.dist_mat.toarray()
+        return dist_mat
+    
     @staticmethod
     def compute_cosine_dist_mat(X):
         "The computation of cosine distance matrix from vectorized instances"
         dot_product = np.dot(X, X.T)
         vec_norms = np.linalg.norm(X, axis=1)
-        norm_product = \
-          np.outer(vec_norms, vec_norms)
+        norm_product = np.outer(vec_norms, vec_norms)
         return dot_product / norm_product
 
-
-class MAL1(ActiveLearner):
-    "This is the very core medoid-based active learning method. Clustering is performed only once here"
-
-    def compute_dist_mat(self):
-        # Distance matrix, the vectors are assumed to be normalized (Norm of embedding vector is 1).
-        # Thus, inner product is equivalent to cosine similarity
-        self.dist_mat = np.dot(X, X.T)
-        self.dist_mat = 1 - self.dist_mat.toarray()
-
-    def clustering(self, K):
-        self.cluster_analyzer = cluster.KMedoidClustering(self.dist_mat, K)
-        self.cluster_analyzer.perform_clustering()
-
-    def draw_next_batch(self):
-        pass
-
+    @staticmethod
+    def clustering(dist_mat, K, order_by="size"):
+        cluster_analyzer = cluster.KMedoidClustering(dist_mat, K)
+        cluster_analyzer.perform_clustering()
+        cluster_analyzer.sort_clusters(order_by=order_by)
+        return cluster_analyzer
     
+    def draw_next_batch(self):
+        "Generate medoids and draw the medoids from a deque."
+        if not hasattr(self, 'dist_mat'):
+            self.dist_mat = self.compute_dist_mat()
+        if not hasattr(self, 'cluster_analyzer'):
+            self.cluster_analyzer = self.clustering(self.dist_mat, self.initial_batch_size)
+            self.medoids = collections.deque(self.cluster_analyzer.medoids)
+        selection_batch = []
+        if hasattr(self, 'medoids'):
+            batch_size = (self.batch_size if self.n_batch == 0
+                          else self.initial_batch_size)
+            for i in range(batch_size):
+                if self.medoids:
+                    selection_batch.append(self.medoids.popleft())
+                else:
+                    break
+                    print("Medoids are exhausted.")
+            return np.array(selection_batch)
+                   
+    def annotate_batch(self, selection_batch, batch_target):
+        "In addition to annotation, propagate the labels from medoids."
+        super(MAL1, self).annotate_batch(selection_batch, batch_target)
+        
+        
+        # self.y[selection_batch] = batch_target
+        # self.L = np.array(self.L.tolist() + selection_batch.tolist())  # Labeled and to be labeled in the same batch
+        # self.U = np.array(list(set(self.U.tolist()) - set(selection_batch.tolist())))
+
+        tmp_P = self.P.tolist()
+        for i, sample in enumerate(selection_batch):
+            self.y[sample] = batch_target[i]
+            for cluster_member in self.cluster_analyzer.clusters[sample]:
+                self.y[cluster_member] = batch_target[i]
+                tmp_P.append(cluster_member)
+        self.P = np.array(tmp_P)
+        
+    def train_with_propagated_labels(self):
+        self.classifier.fit(self.X[self.P], self.y[self.P])
+
+        
+
+
 
 class MismatchFirstFarthestTraversal:
     "The implementation of mismatch-first farthest-traversal"
